45a173495c4c321f9a837894a61ac41b7f13ac4c
alikurb12 <kurbansahatowali@gmail.com>
2025-03-17 21:04:16 +0300

    Update quiz

--- application/tg_bot/training/__init__.py
+++ application/tg_bot/training/__init__.py
@@ -4,12 +4,14 @@ from aiogram import Router
 from application.tg_bot.training.admin_actions.handlers import router as admin_training_handler_routers
 from application.tg_bot.training.personal_actions.handlers import router as training_routers
 from application.tg_bot.training.personal_actions.education import router as education_routers
+from application.tg_bot.training.personal_actions.quiz.handlers import router as quiz_router
 
 router = Router()
 
 router.include_router(admin_training_handler_routers)
 router.include_router(training_routers)
 router.include_router(education_routers)
+router.include_router(quiz_router)
 
 
 
