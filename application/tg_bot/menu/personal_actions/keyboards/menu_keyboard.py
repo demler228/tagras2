3579ae77e840bb5c2a8448d3a0c572ba6eec8009
Andre Shark <aynur_shakirov_2013@mail.ru>
2025-03-24 16:08:06 +0300

    rework admins routing and add admin check by db

--- application/tg_bot/menu/personal_actions/keyboards/menu_keyboard.py
+++ application/tg_bot/menu/personal_actions/keyboards/menu_keyboard.py
@@ -15,7 +15,8 @@ def get_main_menu_keyboard(is_admin: bool) -> InlineKeyboardMarkup:
     builder.button(text="Карты офиса", callback_data="office_maps_button")
     builder.button(text="ИИ - Помощник", callback_data="ai_assistant_button")
     builder.button(text="Тренинги", callback_data="training_button")
-    builder.button(text="Админ-панель", callback_data="admin_main_menu_button")
+    if is_admin:
+        builder.button(text="Админ-панель", callback_data="admin_main_menu_button")
 
     builder.adjust(1)
 
