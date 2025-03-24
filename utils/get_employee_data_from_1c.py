00149008f8380da0f2935f5b08b650b1d5a42c92
Ramzil <yazik_2018@mail.ru>
2025-03-24 18:06:53 +0300

    fixed hourly function and config

--- utils/get_employee_data_from_1c.py
+++ utils/get_employee_data_from_1c.py
@@ -8,7 +8,7 @@ async def hourly_request_to_1c():
     while True:
         try:
             await UserBL.update_employee_data()
-            logger.error(f"Данные о сотрудниках успешно синхронизированы с базой 1с")
+            logger.info(f"Данные о сотрудниках успешно синхронизированы с базой 1с")
 
         except Exception as e:
             logger.error(f"Ошибка при обновлении данных сотрудников: {e}")
