a096c6a44a1da2c0a33d87971e29ca08c24fe27a
ratmir <ratmir666123@gmail.com>
2025-04-11 08:53:45 +0300

    add new directory

---
+++ application/tg_bot/department_list/entities/department_list.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class Department(BaseModel):
+    id: Optional[int] = None
+    name: Optional[str] = None
+
+class Employee(BaseModel):
+    id: Optional[int] = None
+    name: Optional[str] = None
+    phone: Optional[str] = None
+    description: Optional[str] = None
+    department_id: Optional[int] = None
+    department: Optional[Department] = None  # Для чтения связей
