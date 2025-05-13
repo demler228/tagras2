f3c40a4070100cd5b2ba9a2b31738a1298776ed1
Ramzil <yazik_2018@mail.ru>
2025-05-14 01:01:16 +0300

    edited errors

--- application/tg_bot/user/entities/user.py
+++ application/tg_bot/user/entities/user.py
@@ -5,7 +5,7 @@ from typing import Optional
 class User(BaseModel):
     id: int = Field(default=-1)
     telegram_id: Optional[int] = None
-    tg_username: str
-    username: str
-    phone: str
+    tg_username: Optional[str] = None
+    username: Optional[str] = None
+    phone: Optional[str] = None
     role: str = Field(default="user")
